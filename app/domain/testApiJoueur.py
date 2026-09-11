22238c823995b723f04405a26db4b9c937a434f0
pemerick <emerick.pacaud@ynov.com>
2026-09-11 17:03:35 +0200

    Ajout d'une fonction qui permet de demander au joueur combien de personnes doivent être dans la partie

--- app/domain/testApiJoueur.py
+++ app/domain/testApiJoueur.py
@@ -1,44 +1,51 @@
 import requests
 
 baseURL = "http://127.0.0.1:8000"
+débug = False
+
 id_partie = requests.get(f"{baseURL}/").json()
-print("Ma clef : "+id_partie)
+if débug == True :
+    print("Ma clef : "+id_partie)
 
 def requête(url:str):
     url = f"{baseURL}{url}"
     response = requests.get(url)
-    print(f"Réponse : {response.text}\n")
+    if débug == True :
+        print(f"Réponse : {response.text}\n")
 
 
 def ajouter_joueur(payload):
     url = f"{baseURL}/players"
     response = requests.post(url, json=payload)
-    print(f"Réponse : {response.text}\n")
+    if débug == True :
+        print(f"Réponse : {response.text}\n")
 
 
 def supprimer_joueur(player_id:int):
     global id_partie
     url = f"{baseURL}/players/{id_partie}/{player_id}"
     response = requests.delete(url)
-    print(f"Réponse : {response.text}\n")
+    if débug == True :
+        print(f"Réponse : {response.text}\n")
     
 
 def modifier_score_joueur(salle:str, player_id:int, player_score:int):
     global id_partie
     url = f"{baseURL}/players/{id_partie}/{salle}/{player_id}/{player_score}"
     response = requests.put(url)
-    print(f"Réponse : {response.text}\n")
+    if débug == True :
+        print(f"Réponse : {response.text}\n")
 
 # Pour le post :
-ajouter_joueur({"name": "Nom1", "score_salle_1": 0, "score_salle_2": 0, "score_salle_3": 0, "id_partie" : id_partie})
-ajouter_joueur({"name": "Nom2", "score_salle_1": 0, "score_salle_2": 0, "score_salle_3": 0, "id_partie" : id_partie})
-ajouter_joueur({"name": "Nom3", "score_salle_1": 0, "score_salle_2": 0, "score_salle_3": 0, "id_partie" : id_partie})
+# ajouter_joueur({"name": "Nom1", "score_salle_1": 0, "score_salle_2": 0, "score_salle_3": 0, "id_partie" : id_partie})
+# ajouter_joueur({"name": "Nom2", "score_salle_1": 0, "score_salle_2": 0, "score_salle_3": 0, "id_partie" : id_partie})
+# ajouter_joueur({"name": "Nom3", "score_salle_1": 0, "score_salle_2": 0, "score_salle_3": 0, "id_partie" : id_partie})
 
 # # Pour le get :
 # print(requête(f"/players/{id_partie}/2"))
 # print(requête(f"/players/{id_partie}"))
 # print(requête(f"/players/{id_partie}/1"))
-print(requête(f"/id/list/{id_partie}"))
+# print(requête(f"/id/list/{id_partie}"))
 
 
 
@@ -48,3 +55,21 @@ print(requête(f"/id/list/{id_partie}"))
 
 # # Pour le put :
 # modifier_score_joueur("salle-1",1,20)
+
+
+def ajouter_joueur_action():
+    print("♫ Bienvenu ! ♪")
+    print("Combien de joueur vons jouer ? [1 - 50]")
+    nombre = 0
+    while (nombre <= 0 or nombre > 50):
+        nombre = int(input("> "))
+    print("")
+    for i in range(1, nombre+1):
+        print(f"Bienvenu joueur {nombre} ! ♪ Comment te nomme tu ?")
+        nom = ""
+        while (len(nom) < 3 or len(nom) > 20):
+            nom = input("> ")
+        ajouter_joueur({"name": nom, "score_salle_1": 0, "score_salle_2": 0, "score_salle_3": 0, "id_partie" : id_partie})
+        print("")
+
+ajouter_joueur_action()
