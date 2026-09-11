27bb0999795e248a946b02637143951c63421762
pemerick <emerick.pacaud@ynov.com>
2026-09-11 14:23:34 +0200

    Ajout de la gestion du joueur dans l'API

--- api.py
+++ api.py
@@ -1,14 +1,24 @@
 import json
 from fastapi import FastAPI
 import random
+from pydantic import BaseModel, Field
 
 app = FastAPI()
+
+class Player(BaseModel):
+    name: str = Field(min_length=3)
+    score_salle_1: int = Field(ge=0)
+    score_salle_2: int = Field(ge=0)
+    score_salle_3: int = Field(ge=0)
 
 déjà_vu = {
     "salle-1" : [],
     "salle-2" : [],
     "salle-3" : []
 }
+
+players = []
+id_global = 0
 
 def nombre_aléatoire_avec_liste_à_ignorer(liste_origine, liste_à_ignorer):
     liste = []
@@ -80,3 +90,67 @@ def salle_question_id(salle : str, question_id : int) :
         return donnees[int(question_id)]
 
 # --------------------------------------------------------------
+
+def identifiant_position_player(id_chercher : int):
+    for i in range(len(players)):
+        if players[i]["id"] == id_chercher :
+            return i
+    return -1
+
+@app.get("/players")
+def get_players():
+    return players
+
+@app.get("/players/{player_id}")
+def get_player(player_id: int):
+    player_id = identifiant_position_player(player_id)
+    if player_id != -1 :
+        return players[player_id]
+    return {"error": "Player not found"}
+
+@app.get("/id/list")
+def get_list_id():
+    tableau = []
+    for i in range(len(players)):
+        tableau.append(players[i]["id"])
+    return tableau
+
+@app.post("/players")
+def create_player(player: Player):
+    global id_global, players
+    new_player = player.model_dump()
+    new_player["id"] = id_global
+    id_global+=1
+    players.append(new_player)
+    return new_player
+
+@app.delete("/players/{player_id}")
+def delete_player(player_id: int):
+    global players
+    player_id = identifiant_position_player(player_id)
+
+    if player_id != -1 :
+        players.pop(player_id) 
+        return {"message": f"Joueur à l'index {player_id} supprimé"}
+    
+    return {"error": "Joueur non trouvé"}, 404
+
+@app.put("/players/score/{player_id}/{score}")
+def put_players_score(player_id:int, score:int):
+    player_id = identifiant_position_player(player_id)
+
+    if player_id != -1 :
+        players[player_id]["score"] = score
+        return {"message": "Le score du joueur à été mis à jours !"}
+                
+    return {"error": "Joueur non trouvé"}, 404
+
+@app.put("/players/level/{player_id}/{level}")
+def put_player_level(player_id:int, level:int):
+    player_id = identifiant_position_player(player_id)
+
+    if player_id != -1 :
+        players[player_id]["level"] = level
+        return {"message": "Le niveau du joueur à été mis à jours !"}
+
+    return {"error": "Joueur non trouvé"}, 404
